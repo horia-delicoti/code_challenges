7d42cebf147272a0c408e0b06157c1ab353698c8
Horia <horia@Horias-Air.localdomain>
2023-03-24 11:13:48 +0200

    leetcode: 383. Ransom Note

---
+++ leetcode/hash_table/383_ransome_note.py
@@ -0,0 +1,15 @@
+def canConstruct(ransomNote, magazine):
+    arr = ""
+    for element_ransom in ransomNote:
+        for element_magazine in magazine:
+            if element_ransom == element_magazine:
+                arr += element_ransom
+    if arr == ransomNote:
+        return 1
+    else:
+        return 0
+
+ransomNote = "leetcode"
+magazine = "arrayodistlec"
+
+canConstruct(ransomNote, magazine)
