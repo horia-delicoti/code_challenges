ffa6b1fb226691fa0e4bc886bb3c17adc23c38a0
Horia Delicoti <horia.delicoti@gmail.com>
2023-07-28 17:51:41 +0300

    two_sum.py: Update problem

--- leetcode/arrays/two_sum.py
+++ leetcode/arrays/two_sum.py
@@ -7,14 +7,18 @@
 # Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
 def two_sum(nums, target):
-    res = []
+    hashmap = {}
 
     for i in range(len(nums)):
-        for j in range(i+1, len(nums)):
-            if nums[i] + nums[j] == target:
-                return i,j
+        hashmap[nums[i]] = i
+    
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if complement in hashmap:
+            return i, hashmap[complement]
+    
 
-nums = [2,7,11,15]
+nums = [3,2,4]
 target = 9
 
 print(two_sum(nums,target))
