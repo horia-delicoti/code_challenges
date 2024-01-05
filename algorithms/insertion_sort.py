2ac2bed351e1b2ecac4b605c34bdb150b4a4c629
Horia Delicoti <horia.delicoti@gmail.com>
2024-01-05 14:21:48 +0000

    Sorting: Add insert sort algorith

---
+++ algorithms/insertion_sort.py
@@ -0,0 +1,18 @@
+# Insertion sort is a simple sorting algorithm that works similar to the way
+# you sort playing cards in your hands. The array is virtually split into a sorted and an unsorted part.
+# Values from the unsorted part are picked and placed at the correct position in the sorted part.
+
+def insertionSort(arr):
+    for i in range(1, len(arr)):
+        j = i - 1
+        key = arr[i]
+
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+
+arr = [12, 11, 13, 5, 6]
+insertionSort(arr)
+
+print(arr)
