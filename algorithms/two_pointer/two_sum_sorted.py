abc42cc10fab10382ef30170d384ea934af187c3
Horia Delicoti <horia.delicoti@gmail.com>
2024-06-07 10:31:08 +0100

    Two Pointer: Two Sum Sorted

--- algorithms/two_pointer/two_sum_sorted.py
+++ algorithms/two_pointer/two_sum_sorted.py
@@ -10,19 +10,18 @@
 # Output: 1 3
 
 def two_sum_sorted(arr, target):
-    first = 0
-    last = len(arr) - 1
-    
-    while first < last:
-        print('first ', arr[first])
-        print('last ', arr[last])
-        if arr[first] + arr[last] == target:
-            return first, last
-        elif arr[first] + arr[last] > target:
-            last -= 1
+    left = 0
+    right = len(arr) - 1
+
+    while left < right:
+        sum = arr[left] + arr[right]
+
+        if sum == target:
+            return [left, right]
+        if sum > target:
+            right -= 1
         else:
-            if arr[first] + arr[last] < target:
-                first += 1
+            left += 1
 
 arr = [2, 3, 4, 5, 8, 11, 18]
 target = 8
