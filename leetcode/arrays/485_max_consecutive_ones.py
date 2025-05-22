d5f37218904cf538ceeef2f70f4ec75b81ee927e
Horia Delicoti <horia.delicoti@gmail.com>
2025-05-22 14:25:38 +0100

    485. Maximum consecutive ones

--- leetcode/arrays/485_max_consecutive_ones.py
+++ leetcode/arrays/485_max_consecutive_ones.py
@@ -1,18 +1,19 @@
 def findMaxConsecutiveOnes(nums):
-    count = 0
     max_count = 0
-    for i in nums:
-        if i == 1:
-            count += 1
-            print('count = ', count)
-            if max_count < count:
-                max_count = count
-                print('max_count = ', max_count)
-        elif i == 0:
-            count = 0
-    print(max_count)
+    count = 0
 
-nums = [1,1,0,1,1,1]
+    for i in range(len(nums)):
+        if nums[i] == 1:
+            count += 1
+            print("max_count ", max_count)
+            max_count = max(max_count, count)
+            print("max_count after = ", max_count)
+        else:
+            count = 0
+            print("count ", count)
+    return max_count
+
+nums = [1, 1, 0, 1, 1 , 1]
 findMaxConsecutiveOnes(nums)
 
 # Time Complexity: O(n) because we iterate once the array
