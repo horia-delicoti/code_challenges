01d0bafeffe3fcf0e1272ce65d763b37d81dd636
Horia Delicoti <horia.delicoti@gmail.com>
2025-06-09 14:22:48 +0100

    Leetcode Array: 1. Two sum optimized

--- leetcode/arrays/1_two_sum.py
+++ leetcode/arrays/1_two_sum.py
@@ -6,6 +6,9 @@
 # Output: [0,1]
 # Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
+# Brute Force Solution
+# Time Complexity: O(n^2) - The solutions uses a nested loop to check each piar of numbers.
+# Space Complexity: 0(1) - Does not use any additional data structures that grow with input size.
 def two_sum(nums, target):
     n = len(nums)
     for i in range(n):
@@ -15,10 +18,21 @@ def two_sum(nums, target):
             if nums[i] + nums[j] == target:
                 return [i, j]
 
-nums = [2, 7, 11, 15]
+# Optimized Solution using Hash Map
+# Time Complexity: O(n) - The solution uses a single pass through the array.
+# Space Complexity: O(n) - Uses a hash map to store the indices of the numbers.
+def two_sum_optimized(nums, target):
+    nums_dict = {}
+    for i, num in enumerate(nums):
+        print(f"Current index: {i}, Current number: {num}")
+        complement = target - num
+        if complement in nums_dict:
+            print(f"Found complement: {complement} at index {nums_dict[complement]}")
+            return [nums_dict[complement], i]
+        nums_dict[num] = i
+
+nums = [2, 7, 9, 11, 15]
 target = 9
 
 print(two_sum(nums, target))
-
-# Time Complexity: O(n^2) - The solutions uses a nested loop to check each piar of numbers.
-# Space Complexity: 0(1) - Does not use any additional data structures that grow with input size.
+print(two_sum_optimized(nums, target))
