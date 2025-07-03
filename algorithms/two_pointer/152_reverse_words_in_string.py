0999d3fde468e3b17b6c4c62d69a6b37e352570c
Horia Delicoti <horia.delicoti@gmail.com>
2025-07-03 12:49:53 +0100

    Two Pointer: 152. Reverse words in string

---
+++ algorithms/two_pointer/152_reverse_words_in_string.py
@@ -0,0 +1,15 @@
+# Given an input string s, reverse the order of the words.
+# A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+# Return a string of the words in reverse order concatenated by a single space.
+# Note that s may contain leading or trailing spaces or multiple spaces between two words.
+# The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+def reverseWords(s):
+    return " ".join(reversed(s.split()))
+
+s = "the sky is blue"
+
+print(reverseWords(s))
+
+# Time Complexity: O(n), where n is the length of the string s.
+# Space Complexity: O(n), where n is the length of the string s.
