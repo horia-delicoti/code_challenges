ba1716353964def304eaee3048b9c3cf1d7a9f88
Horia Delicoti <horia.delicoti@gmail.com>
2025-07-31 15:17:56 +0100

    List: 1051. Height Checker

---
+++ data_structure/list/1051_height_checker.py
@@ -0,0 +1,23 @@
+# A school is trying to take an annual photo of all the students. The students are asked to stand in a single
+# file line in non-decreasing order by height. Let this ordering be represented by the integer array expected
+# where expected[i] is the expected height of the ith student in line.
+#
+# You are given an integer array heights representing the current order that the students are standing in.
+# Each heights[i] is the height of the ith student in line (0-indexed).
+
+# Return the number of indices where heights[i] != expected[i].
+
+def heightChecker(heights):
+    expected = sorted(heights)
+    count = 0
+    for i in range(len(heights)):
+        if heights[i] != expected[i]:
+            count += 1
+    return count
+
+heights = [1, 1, 4, 2, 1, 3]
+
+print(heightChecker(heights))
+
+# Time Complexity: O(n log n), where n is the length of the heights array due to sorting.
+# Space Complexity: O(n), where n is the length of the heights array due to the creation of the expected array.
